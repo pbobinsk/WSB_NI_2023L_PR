0fb659d26f4247d0c18d8b5c2a46f2390be1e8b6
Piotr Bobiński <piotr.bobinski@pw.edu.pl>
2023-05-27 10:08:24 +0200

    Zmiana z forka

--- src/Testowa.java
+++ src/Testowa.java
@@ -2,5 +2,6 @@ public class Testowa {
     public static void main(String[] args) {
         System.out.println("Testowa");
         System.out.println("zmiana");
+        System.out.println("z forka");
     }
 }
