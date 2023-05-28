0cfb662f6124366e2377056e5bc5002c87ee8955
Piotr Bobiński <piotr.bobinski@wsb.warszawa.pl>
2023-05-28 19:52:14 +0200

    Zajęcia 19:25 - forked

--- src/Godzina1925.java
+++ src/Godzina1925.java
@@ -1,5 +1,7 @@
 public class Godzina1925 {
     public static void main(String[] args) {
+
         System.out.println("Zajęcia 19:25 - cloned");
+        System.out.println("Zajęcia 19:25 - forked");
     }
 }
